a6339ae3888f8322deebef49f0cb47603b1e97d4
Anders Jacobsen <andersmbj@gmail.com>
2013-09-26 15:13:33 -0400

    added tutorial etc.

---
+++ app/controllers/info_controller.rb
@@ -0,0 +1,22 @@
+class InfoController < ApplicationController
+  layout "info"
+  # GET /cancer_types
+  # GET /cancer_types.json
+  def version
+    @cancer_types = CancerType.all
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @cancer_types }
+    end
+  end
+
+  def about
+
+    respond_to do |format|
+      format.html
+    end
+    
+  end
+  
+end
